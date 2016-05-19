0fdacc7efd33dc750112b6c5aef9113eba4a4c0a
roodkapje <nielssteensma@yahoo.nl>
2016-05-19 22:02:31 +0200

    Database controller gemaakt

---
+++ CrmAppSchool/CrmAppSchool/Controllers/DatabaseController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace CrmAppSchool.Controllers
+{
+    public abstract class DatabaseController
+    {
+        protected MySqlConnection conn;
+        public DatabaseController()
+        {
+            conn = new MySqlConnection("Server=172.98.192.163;Database=localhost;Uid=crmapp;Pwd=1234");
+        }
+    }
+}
