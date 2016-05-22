dbcc63c6963ffbd70bec3c8829d112accd890bb9
Daan Nieuwenburg <15070735@student.hhs.nl>
2016-05-22 20:47:08 +0200

    Wat aanpassingen gedaan aan t hoofdmenu

--- CrmAppSchool/CrmAppSchool/Views/Hoofdmenu/HoofdmenuForm.Designer.cs
+++ CrmAppSchool/CrmAppSchool/Views/Hoofdmenu/HoofdmenuForm.Designer.cs
@@ -30,12 +30,16 @@
         {
             this.gebruikerLbl = new System.Windows.Forms.Label();
             this.voegGebruikerToeBtn = new System.Windows.Forms.Button();
+            this.btnZoeken = new System.Windows.Forms.Button();
+            this.btnProfiel = new System.Windows.Forms.Button();
+            this.btnOpdrachten = new System.Windows.Forms.Button();
+            this.btnContacten = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // gebruikerLbl
             // 
             this.gebruikerLbl.AutoSize = true;
-            this.gebruikerLbl.Location = new System.Drawing.Point(12, 484);
+            this.gebruikerLbl.Location = new System.Drawing.Point(25, 387);
             this.gebruikerLbl.Name = "gebruikerLbl";
             this.gebruikerLbl.Size = new System.Drawing.Size(101, 13);
             this.gebruikerLbl.TabIndex = 0;
@@ -43,7 +47,7 @@
             // 
             // voegGebruikerToeBtn
             // 
-            this.voegGebruikerToeBtn.Location = new System.Drawing.Point(121, 313);
+            this.voegGebruikerToeBtn.Location = new System.Drawing.Point(28, 332);
             this.voegGebruikerToeBtn.Name = "voegGebruikerToeBtn";
             this.voegGebruikerToeBtn.Size = new System.Drawing.Size(123, 23);
             this.voegGebruikerToeBtn.TabIndex = 1;
@@ -51,11 +55,68 @@
             this.voegGebruikerToeBtn.UseVisualStyleBackColor = true;
             this.voegGebruikerToeBtn.Click += new System.EventHandler(this.voegGebruikerToeBtn_Click);
             // 
+            // btnZoeken
+            // 
+            this.btnZoeken.BackgroundImage = global::CrmAppSchool.Properties.Resources.button_Zoeken;
+            this.btnZoeken.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnZoeken.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnZoeken.FlatAppearance.BorderSize = 0;
+            this.btnZoeken.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnZoeken.Location = new System.Drawing.Point(188, 39);
+            this.btnZoeken.Name = "btnZoeken";
+            this.btnZoeken.Size = new System.Drawing.Size(139, 115);
+            this.btnZoeken.TabIndex = 3;
+            this.btnZoeken.UseVisualStyleBackColor = true;
+            // 
+            // btnProfiel
+            // 
+            this.btnProfiel.BackgroundImage = global::CrmAppSchool.Properties.Resources.button_Profiel;
+            this.btnProfiel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnProfiel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnProfiel.FlatAppearance.BorderSize = 0;
+            this.btnProfiel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProfiel.Location = new System.Drawing.Point(28, 173);
+            this.btnProfiel.Name = "btnProfiel";
+            this.btnProfiel.Size = new System.Drawing.Size(139, 115);
+            this.btnProfiel.TabIndex = 4;
+            this.btnProfiel.UseVisualStyleBackColor = true;
+            // 
+            // btnOpdrachten
+            // 
+            this.btnOpdrachten.BackgroundImage = global::CrmAppSchool.Properties.Resources.btnOpdrachten;
+            this.btnOpdrachten.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnOpdrachten.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOpdrachten.FlatAppearance.BorderSize = 0;
+            this.btnOpdrachten.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpdrachten.Location = new System.Drawing.Point(188, 173);
+            this.btnOpdrachten.Name = "btnOpdrachten";
+            this.btnOpdrachten.Size = new System.Drawing.Size(139, 115);
+            this.btnOpdrachten.TabIndex = 5;
+            this.btnOpdrachten.UseVisualStyleBackColor = true;
+            // 
+            // btnContacten
+            // 
+            this.btnContacten.BackColor = System.Drawing.Color.Transparent;
+            this.btnContacten.BackgroundImage = global::CrmAppSchool.Properties.Resources.button_Contacten2;
+            this.btnContacten.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnContacten.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnContacten.FlatAppearance.BorderSize = 0;
+            this.btnContacten.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnContacten.Location = new System.Drawing.Point(28, 39);
+            this.btnContacten.Name = "btnContacten";
+            this.btnContacten.Size = new System.Drawing.Size(139, 115);
+            this.btnContacten.TabIndex = 2;
+            this.btnContacten.UseVisualStyleBackColor = false;
+            // 
             // HoofdmenuForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(840, 506);
+            this.ClientSize = new System.Drawing.Size(840, 428);
+            this.Controls.Add(this.btnOpdrachten);
+            this.Controls.Add(this.btnProfiel);
+            this.Controls.Add(this.btnZoeken);
+            this.Controls.Add(this.btnContacten);
             this.Controls.Add(this.voegGebruikerToeBtn);
             this.Controls.Add(this.gebruikerLbl);
             this.Name = "HoofdmenuForm";
@@ -69,5 +130,9 @@
 
         private System.Windows.Forms.Label gebruikerLbl;
         private System.Windows.Forms.Button voegGebruikerToeBtn;
+        private System.Windows.Forms.Button btnContacten;
+        private System.Windows.Forms.Button btnZoeken;
+        private System.Windows.Forms.Button btnProfiel;
+        private System.Windows.Forms.Button btnOpdrachten;
     }
 }
---
+++ CrmAppSchool/CrmAppSchool/Views/Zoeken/ZoekenForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace CrmAppSchool.Views.Zoeken
+{
+    partial class ZoekenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(372, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Filter op:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Voornaam",
+            "Achternaam",
+            "Kwaliteit",
+            "Organisatie",
+            "Locatie",
+            "Functie"});
+            this.comboBox1.Location = new System.Drawing.Point(42, 114);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // ZoekenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 167);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "ZoekenForm";
+            this.Text = "ZoekenForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
