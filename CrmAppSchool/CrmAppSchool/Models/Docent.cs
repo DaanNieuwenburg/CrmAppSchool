ef767e2f44ab2d5e9bb39dfc81ce3e6150fe7833
roodkapje <nielssteensma@yahoo.nl>
2016-05-22 14:26:49 +0200

    Docent erfde nog niet over van gebruiker, nu wel

---
+++ CrmAppSchool/CrmAppSchool/Models/Docent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmAppSchool.Models
+{
+    public class Docent : Gebruiker
+    {
+        public Docent(string _gebruikersnaam)
+        {
+            Gebruikersnaam = _gebruikersnaam;
+        }
+    }
+}
