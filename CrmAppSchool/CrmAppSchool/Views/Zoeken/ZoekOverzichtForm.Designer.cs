ffc062bd3eaf04a389e48d65a81341f10345a0f4
Daan Nieuwenburg <15070735@student.hhs.nl>
2016-05-24 10:26:35 +0200

    ZoekresultatenForm heeft nu Homepb en ingelogd als label

--- CrmAppSchool/CrmAppSchool/Views/Zoeken/ZoekOverzichtForm.Designer.cs
+++ CrmAppSchool/CrmAppSchool/Views/Zoeken/ZoekOverzichtForm.Designer.cs
@@ -35,20 +35,25 @@
             this.functieHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.kwaliteitHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.pbHeader = new System.Windows.Forms.PictureBox();
+            this.pbHome = new System.Windows.Forms.PictureBox();
+            this.lblGebruiker = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbHeader)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHome)).BeginInit();
             this.SuspendLayout();
             // 
             // resultatenLvw
             // 
+            this.resultatenLvw.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.resultatenLvw.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.voornaamHeader,
             this.achternaamHeader,
             this.bedrijfHeader,
             this.functieHeader,
             this.kwaliteitHeader});
-            this.resultatenLvw.Location = new System.Drawing.Point(72, 65);
+            this.resultatenLvw.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultatenLvw.Location = new System.Drawing.Point(12, 65);
             this.resultatenLvw.Name = "resultatenLvw";
-            this.resultatenLvw.Size = new System.Drawing.Size(484, 352);
+            this.resultatenLvw.Size = new System.Drawing.Size(608, 352);
             this.resultatenLvw.TabIndex = 15;
             this.resultatenLvw.UseCompatibleStateImageBehavior = false;
             this.resultatenLvw.View = System.Windows.Forms.View.Details;
@@ -83,21 +88,50 @@
             this.pbHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(183)))), ((int)(((byte)(52)))));
             this.pbHeader.Location = new System.Drawing.Point(-10, -1);
             this.pbHeader.Name = "pbHeader";
-            this.pbHeader.Size = new System.Drawing.Size(664, 60);
+            this.pbHeader.Size = new System.Drawing.Size(664, 50);
             this.pbHeader.TabIndex = 14;
             this.pbHeader.TabStop = false;
+            // 
+            // pbHome
+            // 
+            this.pbHome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(183)))), ((int)(((byte)(52)))));
+            this.pbHome.BackgroundImage = global::CrmAppSchool.Properties.Resources.picture_Home;
+            this.pbHome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbHome.Location = new System.Drawing.Point(2, -1);
+            this.pbHome.Name = "pbHome";
+            this.pbHome.Size = new System.Drawing.Size(61, 50);
+            this.pbHome.TabIndex = 16;
+            this.pbHome.TabStop = false;
+            // 
+            // lblGebruiker
+            // 
+            this.lblGebruiker.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblGebruiker.AutoSize = true;
+            this.lblGebruiker.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(183)))), ((int)(((byte)(52)))));
+            this.lblGebruiker.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGebruiker.ForeColor = System.Drawing.Color.White;
+            this.lblGebruiker.Location = new System.Drawing.Point(418, 18);
+            this.lblGebruiker.Name = "lblGebruiker";
+            this.lblGebruiker.Size = new System.Drawing.Size(123, 16);
+            this.lblGebruiker.TabIndex = 19;
+            this.lblGebruiker.Text = "U bent ingelogd als:";
             // 
             // ZoekOverzichtForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(632, 429);
+            this.Controls.Add(this.lblGebruiker);
+            this.Controls.Add(this.pbHome);
             this.Controls.Add(this.resultatenLvw);
             this.Controls.Add(this.pbHeader);
             this.Name = "ZoekOverzichtForm";
             this.Text = "Gevonde resultaten";
             ((System.ComponentModel.ISupportInitialize)(this.pbHeader)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHome)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -110,5 +144,7 @@
         private System.Windows.Forms.ColumnHeader bedrijfHeader;
         private System.Windows.Forms.ColumnHeader functieHeader;
         private System.Windows.Forms.ColumnHeader kwaliteitHeader;
+        private System.Windows.Forms.PictureBox pbHome;
+        private System.Windows.Forms.Label lblGebruiker;
     }
 }
