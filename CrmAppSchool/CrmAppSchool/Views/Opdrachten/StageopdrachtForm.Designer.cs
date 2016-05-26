93f5439a402242a4172c7f132f2cf5be5c9bfd21
Daan Nieuwenburg <15070735@student.hhs.nl>
2016-05-26 11:55:12 +0200

    Icon toegevoegd aan Opdrachtenform

--- CrmAppSchool/CrmAppSchool/Views/Opdrachten/StageopdrachtForm.Designer.cs
+++ CrmAppSchool/CrmAppSchool/Views/Opdrachten/StageopdrachtForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StageopdrachtForm));
             this.bZoek = new System.Windows.Forms.Button();
             this.tbZoek = new System.Windows.Forms.TextBox();
             this.lbStage = new System.Windows.Forms.ListBox();
@@ -35,9 +36,10 @@
             // 
             // bZoek
             // 
-            this.bZoek.Location = new System.Drawing.Point(309, 39);
+            this.bZoek.Location = new System.Drawing.Point(232, 32);
+            this.bZoek.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bZoek.Name = "bZoek";
-            this.bZoek.Size = new System.Drawing.Size(75, 23);
+            this.bZoek.Size = new System.Drawing.Size(56, 19);
             this.bZoek.TabIndex = 0;
             this.bZoek.Text = "Zoek";
             this.bZoek.UseVisualStyleBackColor = true;
@@ -45,29 +47,32 @@
             // 
             // tbZoek
             // 
-            this.tbZoek.Location = new System.Drawing.Point(35, 40);
+            this.tbZoek.Location = new System.Drawing.Point(26, 32);
+            this.tbZoek.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbZoek.Name = "tbZoek";
-            this.tbZoek.Size = new System.Drawing.Size(248, 22);
+            this.tbZoek.Size = new System.Drawing.Size(187, 20);
             this.tbZoek.TabIndex = 1;
             // 
             // lbStage
             // 
             this.lbStage.FormattingEnabled = true;
-            this.lbStage.ItemHeight = 16;
-            this.lbStage.Location = new System.Drawing.Point(35, 100);
+            this.lbStage.Location = new System.Drawing.Point(26, 81);
+            this.lbStage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lbStage.Name = "lbStage";
-            this.lbStage.Size = new System.Drawing.Size(248, 196);
+            this.lbStage.Size = new System.Drawing.Size(187, 160);
             this.lbStage.TabIndex = 2;
             this.lbStage.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // StageopdrachtForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(582, 458);
+            this.ClientSize = new System.Drawing.Size(436, 372);
             this.Controls.Add(this.lbStage);
             this.Controls.Add(this.tbZoek);
             this.Controls.Add(this.bZoek);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "StageopdrachtForm";
             this.Text = "StageopdrachtForm";
             this.ResumeLayout(false);
