874a7f3e6667d6f889b0713df53604d5f4a53718
roodkapje <nielssteensma@yahoo.nl>
2016-06-01 20:41:48 +0200

    W8 worden nu via SHA512 Base 64 opgeslagen in DB

---
+++ CrmAppSchool/CrmAppSchool/Controllers/EncryptieController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace CrmAppSchool.Controllers
+{
+    public class EncryptieController
+    {
+
+        public string encrypt(string _string)
+        {
+            SHA512Managed HashTool = new SHA512Managed();
+            byte[] PhraseAsByte = System.Text.Encoding.UTF8.GetBytes(string.Concat(_string));
+            byte[] EncryptedBytes = HashTool.ComputeHash(PhraseAsByte);
+            HashTool.Clear();
+            return Convert.ToBase64String(EncryptedBytes);
+        }
+    }
+}
