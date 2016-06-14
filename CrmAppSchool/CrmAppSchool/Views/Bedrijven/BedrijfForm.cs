99656d26431aae1a258e518beac4991abf8f6825
Daan Nieuwenburg <15070735@student.hhs.nl>
2016-06-14 13:20:03 +0200

    Zoeken werkt nu in bedrijfsForm

--- CrmAppSchool/CrmAppSchool/Views/Bedrijven/BedrijfForm.cs
+++ CrmAppSchool/CrmAppSchool/Views/Bedrijven/BedrijfForm.cs
@@ -22,7 +22,7 @@ namespace CrmAppSchool.Views.Bedrijven
         private bool validmobiel { get; set; }
         private bool validbedrijfemail { get; set; }
         public Gebruiker _gebruiker { get; set; }
-        ContactenController cc = new ContactenController();
+        BedrijfController cc = new BedrijfController();
         public BedrijfForm(Gebruiker _gebruiker)
         {
             InitializeComponent();
@@ -66,19 +66,13 @@ namespace CrmAppSchool.Views.Bedrijven
         {
             lvBedrijven.Items.Clear();
             string input = "%" + tbSearch.Text + "%";
-            List<Persooncontact> resultaten = cc.ZoekContacten(input, _gebruiker);
-            foreach (Persooncontact contact in resultaten)
+            List<Bedrijfcontact> resultaten = cc.ZoekBedrijven(input, _gebruiker);
+            foreach (Bedrijfcontact contact in resultaten)
             {
-                ListViewItem lvi = new ListViewItem(contact.Voornaam + " " + contact.Achternaam);
+                ListViewItem lvi = new ListViewItem(contact.Bedrijfnaam);
+                lvi.SubItems.Add(contact.Hoofdlocatie);
                 lvBedrijven.Items.Add(lvi);
-                if (contact.Isgastdocent == true)
-                {
-                    lvi.ImageKey = "GD";
-                }
-                if (contact.Isstagebegeleider == true)
-                {
-                    lvi.ImageKey = "GS";
-                }
+                lvi.ImageKey = "BD";
 
             }
         }
