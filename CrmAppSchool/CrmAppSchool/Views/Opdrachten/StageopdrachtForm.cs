e7fa44ded6bc051cb7c7908f699ae3a70aea8132
RickSchoot <15090264@student.hhs.nl>
2016-06-15 12:01:26 +0200

    commit

--- CrmAppSchool/CrmAppSchool/Views/Opdrachten/StageopdrachtForm.cs
+++ CrmAppSchool/CrmAppSchool/Views/Opdrachten/StageopdrachtForm.cs
@@ -33,7 +33,7 @@ namespace CrmAppSchool.Views.Opdrachten
                 btnVoegtoe.Visible = false;
                 btnWijzig.Visible = false;
             }
-            Cursor = Cursors.Default;
+            
         }
 
 
@@ -262,12 +262,6 @@ namespace CrmAppSchool.Views.Opdrachten
                     }
                 }
             }
-        }
-
-        private void StageopdrachtForm_Load(object sender, EventArgs e)
-        {
-            
-            
         }
     }
 }
