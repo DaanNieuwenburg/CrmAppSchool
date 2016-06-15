35bd4bdcc0796745511c967558e684c1f8e84c27
Daan Nieuwenburg <15070735@student.hhs.nl>
2016-06-15 14:53:51 +0200

    Bedrijf en contactdetails hebben nu een minimumsize

--- CrmAppSchool/CrmAppSchool/Views/Bedrijven/BedrijfBewerk.Designer.cs
+++ CrmAppSchool/CrmAppSchool/Views/Bedrijven/BedrijfBewerk.Designer.cs
@@ -292,6 +292,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(725, 394);
             this.Controls.Add(this.pnbedrijf2);
             this.Controls.Add(this.bedrijfPnl);
