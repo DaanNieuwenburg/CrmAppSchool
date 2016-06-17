fe4500287afaf96e52a6a68685ad54cac7587c5e
Daan Nieuwenburg <15070735@student.hhs.nl>
2016-06-17 12:53:46 +0200

    Knoppen staan in een panel

--- CrmAppSchool/CrmAppSchool/Views/Hoofdmenu/HoofdmenuForm.Designer.cs
+++ CrmAppSchool/CrmAppSchool/Views/Hoofdmenu/HoofdmenuForm.Designer.cs
@@ -50,11 +50,13 @@
             this.voegGebruikerToeBtn = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.BedrijvenLbl = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pbLogo2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbLogo1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbHome)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbHeader)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // gebruikerLbl
@@ -75,7 +77,7 @@
             this.lblContacten.AutoSize = true;
             this.lblContacten.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblContacten.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(183)))), ((int)(((byte)(52)))));
-            this.lblContacten.Location = new System.Drawing.Point(76, 215);
+            this.lblContacten.Location = new System.Drawing.Point(82, 160);
             this.lblContacten.Name = "lblContacten";
             this.lblContacten.Size = new System.Drawing.Size(67, 16);
             this.lblContacten.TabIndex = 13;
@@ -87,7 +89,7 @@
             this.lblZoeken.AutoSize = true;
             this.lblZoeken.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblZoeken.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(183)))), ((int)(((byte)(52)))));
-            this.lblZoeken.Location = new System.Drawing.Point(261, 215);
+            this.lblZoeken.Location = new System.Drawing.Point(267, 160);
             this.lblZoeken.Name = "lblZoeken";
             this.lblZoeken.Size = new System.Drawing.Size(50, 16);
             this.lblZoeken.TabIndex = 14;
@@ -99,7 +101,7 @@
             this.lblRegistreer.AutoSize = true;
             this.lblRegistreer.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblRegistreer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(183)))), ((int)(((byte)(52)))));
-            this.lblRegistreer.Location = new System.Drawing.Point(50, 373);
+            this.lblRegistreer.Location = new System.Drawing.Point(56, 318);
             this.lblRegistreer.Name = "lblRegistreer";
             this.lblRegistreer.Size = new System.Drawing.Size(110, 16);
             this.lblRegistreer.TabIndex = 15;
@@ -111,7 +113,7 @@
             this.lblProfiel.AutoSize = true;
             this.lblProfiel.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblProfiel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(183)))), ((int)(((byte)(52)))));
-            this.lblProfiel.Location = new System.Drawing.Point(74, 373);
+            this.lblProfiel.Location = new System.Drawing.Point(80, 318);
             this.lblProfiel.Name = "lblProfiel";
             this.lblProfiel.Size = new System.Drawing.Size(72, 16);
             this.lblProfiel.TabIndex = 16;
@@ -123,7 +125,7 @@
             this.lblOpdrachten.AutoSize = true;
             this.lblOpdrachten.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblOpdrachten.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(183)))), ((int)(((byte)(52)))));
-            this.lblOpdrachten.Location = new System.Drawing.Point(249, 373);
+            this.lblOpdrachten.Location = new System.Drawing.Point(255, 318);
             this.lblOpdrachten.Name = "lblOpdrachten";
             this.lblOpdrachten.Size = new System.Drawing.Size(75, 16);
             this.lblOpdrachten.TabIndex = 17;
@@ -135,7 +137,7 @@
             this.lblUitloggen.AutoSize = true;
             this.lblUitloggen.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblUitloggen.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(183)))), ((int)(((byte)(52)))));
-            this.lblUitloggen.Location = new System.Drawing.Point(444, 377);
+            this.lblUitloggen.Location = new System.Drawing.Point(450, 322);
             this.lblUitloggen.Name = "lblUitloggen";
             this.lblUitloggen.Size = new System.Drawing.Size(62, 16);
             this.lblUitloggen.TabIndex = 18;
@@ -150,7 +152,7 @@
             this.btnBedrijven.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnBedrijven.FlatAppearance.BorderSize = 0;
             this.btnBedrijven.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnBedrijven.Location = new System.Drawing.Point(397, 75);
+            this.btnBedrijven.Location = new System.Drawing.Point(403, 20);
             this.btnBedrijven.Name = "btnBedrijven";
             this.btnBedrijven.Size = new System.Drawing.Size(168, 139);
             this.btnBedrijven.TabIndex = 19;
@@ -165,7 +167,7 @@
             this.btnHelp.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnHelp.FlatAppearance.BorderSize = 0;
             this.btnHelp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnHelp.Location = new System.Drawing.Point(571, 57);
+            this.btnHelp.Location = new System.Drawing.Point(572, 6);
             this.btnHelp.Name = "btnHelp";
             this.btnHelp.Size = new System.Drawing.Size(55, 51);
             this.btnHelp.TabIndex = 12;
@@ -181,7 +183,7 @@
             this.btnContacten.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnContacten.FlatAppearance.BorderSize = 0;
             this.btnContacten.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnContacten.Location = new System.Drawing.Point(24, 75);
+            this.btnContacten.Location = new System.Drawing.Point(30, 18);
             this.btnContacten.Name = "btnContacten";
             this.btnContacten.Size = new System.Drawing.Size(168, 139);
             this.btnContacten.TabIndex = 2;
@@ -194,7 +196,7 @@
             this.pbLogo2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(183)))), ((int)(((byte)(52)))));
             this.pbLogo2.BackgroundImage = global::CrmAppSchool.Properties.Resources.Logo_HHS3;
             this.pbLogo2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pbLogo2.Location = new System.Drawing.Point(633, 320);
+            this.pbLogo2.Location = new System.Drawing.Point(633, 323);
             this.pbLogo2.Name = "pbLogo2";
             this.pbLogo2.Size = new System.Drawing.Size(207, 108);
             this.pbLogo2.TabIndex = 11;
@@ -221,7 +223,7 @@
             this.btnUitloggen.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnUitloggen.FlatAppearance.BorderSize = 0;
             this.btnUitloggen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnUitloggen.Location = new System.Drawing.Point(395, 235);
+            this.btnUitloggen.Location = new System.Drawing.Point(401, 180);
             this.btnUitloggen.MinimumSize = new System.Drawing.Size(169, 139);
             this.btnUitloggen.Name = "btnUitloggen";
             this.btnUitloggen.Size = new System.Drawing.Size(169, 139);
@@ -259,7 +261,7 @@
             this.btnOpdrachten.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnOpdrachten.FlatAppearance.BorderSize = 0;
             this.btnOpdrachten.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnOpdrachten.Location = new System.Drawing.Point(203, 231);
+            this.btnOpdrachten.Location = new System.Drawing.Point(209, 176);
             this.btnOpdrachten.Name = "btnOpdrachten";
             this.btnOpdrachten.Size = new System.Drawing.Size(168, 139);
             this.btnOpdrachten.TabIndex = 5;
@@ -274,9 +276,9 @@
             this.btnProfiel.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnProfiel.FlatAppearance.BorderSize = 0;
             this.btnProfiel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnProfiel.Location = new System.Drawing.Point(24, 231);
+            this.btnProfiel.Location = new System.Drawing.Point(30, 176);
             this.btnProfiel.Name = "btnProfiel";
-            this.btnProfiel.Size = new System.Drawing.Size(168, 139);
+            this.btnProfiel.Size = new System.Drawing.Size(6, 6);
             this.btnProfiel.TabIndex = 4;
             this.btnProfiel.UseVisualStyleBackColor = true;
             this.btnProfiel.Click += new System.EventHandler(this.btnProfiel_Click);
@@ -289,7 +291,7 @@
             this.btnZoeken.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnZoeken.FlatAppearance.BorderSize = 0;
             this.btnZoeken.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnZoeken.Location = new System.Drawing.Point(203, 75);
+            this.btnZoeken.Location = new System.Drawing.Point(209, 20);
             this.btnZoeken.Name = "btnZoeken";
             this.btnZoeken.Size = new System.Drawing.Size(168, 139);
             this.btnZoeken.TabIndex = 3;
@@ -305,7 +307,7 @@
             this.voegGebruikerToeBtn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.voegGebruikerToeBtn.FlatAppearance.BorderSize = 0;
             this.voegGebruikerToeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.voegGebruikerToeBtn.Location = new System.Drawing.Point(23, 231);
+            this.voegGebruikerToeBtn.Location = new System.Drawing.Point(29, 176);
             this.voegGebruikerToeBtn.Name = "voegGebruikerToeBtn";
             this.voegGebruikerToeBtn.Size = new System.Drawing.Size(169, 139);
             this.voegGebruikerToeBtn.TabIndex = 1;
@@ -318,7 +320,7 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Right;
             this.pictureBox1.Location = new System.Drawing.Point(633, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(207, 428);
+            this.pictureBox1.Size = new System.Drawing.Size(207, 431);
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
             // 
@@ -327,39 +329,48 @@
             this.BedrijvenLbl.AutoSize = true;
             this.BedrijvenLbl.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BedrijvenLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(183)))), ((int)(((byte)(52)))));
-            this.BedrijvenLbl.Location = new System.Drawing.Point(451, 215);
+            this.BedrijvenLbl.Location = new System.Drawing.Point(457, 160);
             this.BedrijvenLbl.Name = "BedrijvenLbl";
             this.BedrijvenLbl.Size = new System.Drawing.Size(60, 16);
             this.BedrijvenLbl.TabIndex = 20;
             this.BedrijvenLbl.Text = "Bedrijven";
             this.BedrijvenLbl.Visible = false;
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnHelp);
+            this.panel1.Controls.Add(this.btnUitloggen);
+            this.panel1.Controls.Add(this.BedrijvenLbl);
+            this.panel1.Controls.Add(this.voegGebruikerToeBtn);
+            this.panel1.Controls.Add(this.btnBedrijven);
+            this.panel1.Controls.Add(this.btnZoeken);
+            this.panel1.Controls.Add(this.lblUitloggen);
+            this.panel1.Controls.Add(this.btnProfiel);
+            this.panel1.Controls.Add(this.lblOpdrachten);
+            this.panel1.Controls.Add(this.btnOpdrachten);
+            this.panel1.Controls.Add(this.lblProfiel);
+            this.panel1.Controls.Add(this.btnContacten);
+            this.panel1.Controls.Add(this.lblRegistreer);
+            this.panel1.Controls.Add(this.lblZoeken);
+            this.panel1.Controls.Add(this.lblContacten);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(633, 381);
+            this.panel1.TabIndex = 21;
+            // 
             // HoofdmenuForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(840, 428);
-            this.Controls.Add(this.BedrijvenLbl);
-            this.Controls.Add(this.btnBedrijven);
-            this.Controls.Add(this.lblUitloggen);
-            this.Controls.Add(this.lblOpdrachten);
-            this.Controls.Add(this.lblProfiel);
-            this.Controls.Add(this.lblRegistreer);
-            this.Controls.Add(this.lblZoeken);
-            this.Controls.Add(this.lblContacten);
-            this.Controls.Add(this.btnHelp);
-            this.Controls.Add(this.btnContacten);
+            this.ClientSize = new System.Drawing.Size(840, 431);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pbLogo2);
             this.Controls.Add(this.pbLogo1);
-            this.Controls.Add(this.btnUitloggen);
             this.Controls.Add(this.pbHome);
             this.Controls.Add(this.gebruikerLbl);
             this.Controls.Add(this.pbHeader);
-            this.Controls.Add(this.btnOpdrachten);
-            this.Controls.Add(this.btnProfiel);
-            this.Controls.Add(this.btnZoeken);
-            this.Controls.Add(this.voegGebruikerToeBtn);
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(854, 465);
@@ -367,11 +378,14 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hoofdmenu";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.HoofdmenuForm_FormClosed);
+            this.Load += new System.EventHandler(this.HoofdmenuForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pbLogo2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbLogo1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbHome)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbHeader)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -400,5 +414,6 @@
         private System.Windows.Forms.Label lblUitloggen;
         private System.Windows.Forms.Button btnBedrijven;
         private System.Windows.Forms.Label BedrijvenLbl;
+        private System.Windows.Forms.Panel panel1;
     }
 }
